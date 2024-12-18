05740e2c2dad3fd39714d1ee87665e339021d999
Daan Schoenaers <daan.schoenaers@student.ucll.be>
2024-12-18 13:58:26 +0100

    fixed register

--- back-end/model/user.ts
+++ back-end/model/user.ts
@@ -75,20 +75,20 @@ export class User {
         password: string;
     }) {
         if (!age) throw new Error('Age is required');
-        if (!name) throw new Error('Name is required');
-        if (!email) throw new Error('Email is required');
-        if (!password) throw new Error('Password is required');
+        if (!name?.trim()) throw new Error('Name is required');
+        if (!email?.trim()) throw new Error('Email is required');
+        if (!password?.trim()) throw new Error('Password is required');
     }
 
     toPlainObject(): {
-    age: number;
-    name: string;
-    country: string;
-    description: string;
-    email: string;
-    password: string;
-    role?: Role | undefined;
-    teamId?: number | null;
+        age: number;
+        name: string;
+        country: string;
+        description: string;
+        email: string;
+        password: string;
+        role?: Role | undefined;
+        teamId?: number | null;
     } {
         return {
             age: this.age,
@@ -111,18 +111,17 @@ export class User {
         password: string;
         role?: Role | undefined;
     }): boolean {
-    return (
-        this.getAge() === user.age &&
-        this.getName() === user.name &&
-        this.getCountry() === user.country &&
-        this.getDescription() === user.description &&
-        this.getEmail() === user.email &&
-        this.getPassword() === user.password &&
-        this.getRole() === user.role
-    );
+        return (
+            this.getAge() === user.age &&
+            this.getName() === user.name &&
+            this.getCountry() === user.country &&
+            this.getDescription() === user.description &&
+            this.getEmail() === user.email &&
+            this.getPassword() === user.password &&
+            this.getRole() === user.role
+        );
     }
 
-    
     getAge() {
         return this.age;
     }
