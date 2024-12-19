f3a1423ccc7f9669f6ffd675c3ee1c3f6813c53d
Daan Schoenaers <daan.schoenaers@student.ucll.be>
2024-12-19 13:37:44 +0100

    i18n

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -1,12 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { appWithTranslation } from "next-i18next";
 import Header from "@/components/header";
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Header />
       <Component {...pageProps} />
     </>
   );
-}
+};
+
+export default appWithTranslation(App);
